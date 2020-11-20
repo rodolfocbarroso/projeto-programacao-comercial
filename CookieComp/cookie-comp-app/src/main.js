0bfca2ad7c1a20b42cc834dfbd2996ad7ec73686
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-20 00:02:12 -0300

    🔧 Adiciona vuetify e vuex na instancia do vue

--- CookieComp/cookie-comp-app/src/main.js
+++ CookieComp/cookie-comp-app/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import store from './config/store'
+
 
 Vue.config.productionTip = false
 
-new Vue({
+window.app = new Vue({
+  store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
