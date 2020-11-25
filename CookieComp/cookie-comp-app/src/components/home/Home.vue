cd6d96967531739fb81e1addc857a853cef11a6f
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-24 23:34:24 -0300

    ✅ Adiciona componente home

---
+++ CookieComp/cookie-comp-app/src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <PageTitle icon="mdi-home"  main="Dashboard" sub="Cookie Computação" />
+    </div>
+  
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'Home',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
