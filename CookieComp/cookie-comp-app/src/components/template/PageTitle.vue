e6abc2a68913beff53e8f77268672012b8f4df62
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-24 23:33:44 -0300

    ✅ Adiciona componente pagetitle

---
+++ CookieComp/cookie-comp-app/src/components/template/PageTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-title">
+    <h1>
+      <v-icon v-if="icon">{{ icon }}</v-icon> {{ main }}
+    </h1>
+    <h2>{{ sub }}</h2>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageTitle",
+  props: ["icon", "main", "sub"],
+};
+</script>
+
+<style>
+.page-title h1 {
+  margin: 0px;
+}
+.page-title h2 {
+  margin-top: 5px;
+  font-size: 1.3rem;
+}
+</style>
