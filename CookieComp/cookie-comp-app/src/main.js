b814e499208162120aa304d694048f92554f917c
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-24 23:56:52 -0300

    🔧 Adiciona config vuerouter

--- CookieComp/cookie-comp-app/src/main.js
+++ CookieComp/cookie-comp-app/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import store from './config/store'
 
+import vuetify from './plugins/vuetify'
+
+import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
 window.app = new Vue({
   store,
+  router,
   vuetify,
   render: h => h(App),
 }).$mount('#app')
