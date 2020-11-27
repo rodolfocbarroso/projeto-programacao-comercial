96f92b9a53426c36387677105a3442cddda838ac
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-26 23:40:35 -0300

    ✅ Adiciona componente AdminPages

---
+++ CookieComp/cookie-comp-app/src/components/admin/AdminPages.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-pages">
+      <PageTitle icon="mdi-cogs" main="Gerenciador" sub="Administração do Sistema">
+  </div>
+</template>
+
+<script>
+import PageTitle from "../template/PageTitle.vue";
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle },
+};
+</script>
+
+<style>
+</style>
