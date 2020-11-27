022273dfbefffbea2be006d3fc498c78bb605597
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-11-27 00:28:18 -0300

    ✅ Adiciona link para pagina de home

--- CookieComp/cookie-comp-app/src/components/template/Header.vue
+++ CookieComp/cookie-comp-app/src/components/template/Header.vue
@@ -1,29 +1,36 @@
 <template>
   <header>
     <v-app-bar app data-app>
-      <v-app-bar-nav-icon v-if="!hideToggle" @click="toggleMenu"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        v-if="!hideToggle"
+        @click="toggleMenu"
+      ></v-app-bar-nav-icon>
 
       <v-spacer>
         <v-toolbar-title>
-          {{ title }}
+          <div class="title">
+            <router-link to="/">
+              {{ title }}
+            </router-link>
+          </div>
         </v-toolbar-title>
       </v-spacer>
-      
-      <UserDropdown v-if="!hideUserDropdown"/>
+
+      <UserDropdown v-if="!hideUserDropdown" />
     </v-app-bar>
   </header>
 </template>
 
 <script>
-import UserDropdown from './UserDropdown'
+import UserDropdown from "./UserDropdown";
 
 export default {
   name: "Header",
-  components: {UserDropdown },
+  components: { UserDropdown },
   props: {
     title: String,
     hideToggle: Boolean,
-    hideUserDropdown: Boolean
+    hideUserDropdown: Boolean,
   },
   methods: {
     toggleMenu() {
@@ -34,4 +41,8 @@ export default {
 </script>
 
 <style>
+.title a {
+  color: #3b3b3b;
+  text-decoration: none;
+}
 </style>
