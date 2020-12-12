35a08490712876c3ba9b486f5a43aff9ea09ed72
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-12 19:47:07 -0300

    ✅ Adiciona model de categoria

--- CookieCompApi/core/models.py
+++ CookieCompApi/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    ativa = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
