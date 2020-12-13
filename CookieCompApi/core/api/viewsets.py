6b55a8bd9186135d9095e9883258b0fb86624839
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-13 00:26:56 -0300

    ✅ Adiciona viewset de artigo

--- CookieCompApi/core/api/viewsets.py
+++ CookieCompApi/core/api/viewsets.py
@@ -1,9 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import Categoria
 from .serializers import CategoriaSerializer
+from core.models import Artigo
+from .serializers import ArtigoSerializer
 
 
 class CategoriaViewSet(ModelViewSet):
 
     queryset = Categoria.objects.all().order_by('nome')
     serializer_class = CategoriaSerializer
+
+
+class ArtigoViewSet(ModelViewSet):
+
+    queryset = Artigo.objects.all().order_by('data')
+    serializer_class = ArtigoSerializer
