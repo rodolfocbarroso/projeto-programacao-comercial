a4a93d967f80cc3ca43391f6044e9dc872aa92e2
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-13 00:26:14 -0300

    ✅ Adiciona serializer de artigo

--- CookieCompApi/core/api/serializers.py
+++ CookieCompApi/core/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import Categoria
+from core.models import Artigo
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = ('id', 'nome', 'descricao', 'categoriaPai')
+
+
+class ArtigoSerializer(ModelSerializer):
+    class Meta:
+        model = Artigo
+        fields = ('id', 'nome', 'descricao', 'imagemUrl',
+                  'conteudo', 'data', 'usuario', 'categoria',
+                  'comentarios', 'avaliacoes')
