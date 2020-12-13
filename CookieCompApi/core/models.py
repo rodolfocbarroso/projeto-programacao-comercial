cd48c81692fa4d220c5a3c8cf91a50a74b222b21
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-12 21:23:27 -0300

    ✅ Adiciona relacao um pra um de categoria com categoria

--- CookieCompApi/core/models.py
+++ CookieCompApi/core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
+    categoriaPai = models.ForeignKey('self', unique=False, null=True, on_delete=models.SET_NULL, related_name="filhoCategoria")
     ativa = models.BooleanField(default=False)
 
     def __str__(self):
