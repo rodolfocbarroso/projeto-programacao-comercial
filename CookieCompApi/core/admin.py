300acfe96ac9f55d9fa36366ac0f66e6767424a9
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-12 21:08:41 -0300

    ✅ Adiciona model de artigo

--- CookieCompApi/core/admin.py
+++ CookieCompApi/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Categoria
+from .models import Artigo
 
 admin.site.register(Categoria)
+admin.site.register(Artigo)
--- CookieCompApi/core/models.py
+++ CookieCompApi/core/models.py
@@ -1,9 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     ativa = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Artigo(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    imagemUrl = models.CharField(max_length=1000)
+    conteudo = models.BinaryField()
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    aprovado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
