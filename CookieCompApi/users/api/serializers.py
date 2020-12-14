f1a297db0776eaf2c697a43ac2721d8b447797cb
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-14 18:51:58 -0300

    ✅ Adiciona serializer de user

--- CookieCompApi/users/api/serializers.py
+++ CookieCompApi/users/api/serializers.py
@@ -1,1 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
 
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'groups')
