c2a4df91d865aee52581395d5aca82a0bc0521ba
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-13 22:08:37 -0300

    ✅ Adiciona authentication e permission para viewset de avaliacao

--- CookieCompApi/avaliacoes/api/viewsets.py
+++ CookieCompApi/avaliacoes/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from avaliacoes.models import Avaliacao
@@ -9,5 +11,7 @@ class AvaliacaoViewSet(ModelViewSet):
     queryset = Avaliacao.objects.all().order_by('data')
     serializer_class = AvaliacaoSerializer
     filter_backends = [SearchFilter]
+    permission_classes = [DjangoModelPermissions]
+    authentication_classes = [TokenAuthentication]
 
     search_fields = ['=usuario__username']
