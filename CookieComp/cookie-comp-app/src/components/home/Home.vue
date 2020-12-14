230cffe54a925cd4408441c76d9fd9c11993e7e0
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-14 20:53:34 -0300

    ✅ Adiciona componente Stat no componente Home

--- CookieComp/cookie-comp-app/src/components/home/Home.vue
+++ CookieComp/cookie-comp-app/src/components/home/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="home">
     <PageTitle icon="mdi-home" main="Dashboard" sub="Cookie Computação" />
+    <div class="stats">
+            <Stat title="Categorias" :value="0"
+                icon="mdi-folder" color="#d54d50" />
+            <Stat title="Artigos" :value="0"
+                icon="mdi-newspaper-variant-outline" color="#3bc480" />
+            <Stat title="Usuários" :value="0"
+                icon="mdi-account" color="#3282cd" />
+        </div>
   </div>
 </template>
 
 <script>
 import PageTitle from "../template/PageTitle.vue";
+import Stat from './Stat'
 
 export default {
   name: "Home",
-  components: { PageTitle },
+  components: { PageTitle, Stat },
+  data: function() {
+    return {
+      stat: {}
+    }
+  }
 };
 </script>
 
 <style>
+.stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
