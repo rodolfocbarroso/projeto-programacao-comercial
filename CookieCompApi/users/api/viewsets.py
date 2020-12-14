18a4d7500a695b5452438f69aa553e1105580865
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-14 19:30:37 -0300

    ✅ Adiciona viewset de group de users

--- CookieCompApi/users/api/viewsets.py
+++ CookieCompApi/users/api/viewsets.py
@@ -1,1 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth.models import Group
+from .serializers import UserSerializer
+
+
+class GroupViewSet(ModelViewSet):
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = UserSerializer
 
