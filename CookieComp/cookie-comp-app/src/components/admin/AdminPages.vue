24826ef0bc3dbf2d51969718eb637d8c2c7bfcd2
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-14 21:39:15 -0300

    ✅ Adiciona componentes UserAdmin, CategoryAdmin e ArticleAdmin em AdminPages

--- CookieComp/cookie-comp-app/src/components/admin/AdminPages.vue
+++ CookieComp/cookie-comp-app/src/components/admin/AdminPages.vue
@@ -5,15 +5,40 @@
       main="Gerenciador"
       sub="Administração do Sistema"
     />
+    <div class="admin-pages-tabs">
+      <v-tabs>
+        <v-tab>Artigos</v-tab>
+        <v-tab-item>
+          <ArticleAdmin />
+        </v-tab-item>
+        <v-tab>Categorias</v-tab>
+         <v-tab-item>
+          <CategoryAdmin />
+        </v-tab-item>
+        <v-tab>Usuários</v-tab>
+         <v-tab-item>
+          <UserAdmin />
+        </v-tab-item>
+      </v-tabs>
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from "../template/PageTitle.vue";
+import ArticleAdmin from "./ArticleAdmin";
+import CategoryAdmin from "./CategoryAdmin";
+import UserAdmin from "./UserAdmin";
 
 export default {
   name: "AdminPages",
-  components: { PageTitle },
+  components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin },
+  data() {
+    return {
+      tab: null,
+      items: ["web", "shopping", "videos", "images", "news"],
+    };
+  },
 };
 </script>
 
