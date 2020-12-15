b00c3baacd87213f64806e398c26ea86ced42dd8
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2020-12-14 21:57:22 -0300

    🔧Adiciona configuracao de tratamento de erros vindos do backend

--- CookieComp/cookie-comp-app/src/global.js
+++ CookieComp/cookie-comp-app/src/global.js
@@ -1,1 +1,16 @@
+import Vue from 'vue'
+
+
 export const baseApiUrl = 'http://127.0.0.1:8000'
+
+export function showError(e) {
+    if(e && e.response && e.response.data) {
+        Vue.toasted.global.defaultError({ msg : e.response.data })
+    } else if(typeof e === 'string') {
+        Vue.toasted.global.defaultError({ msg : e })
+    } else {
+        Vue.toasted.global.defaultError()
+    }
+}
+
+export default { baseApiUrl, showError }
